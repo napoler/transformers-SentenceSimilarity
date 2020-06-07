91b5a9c3c27f44d0bfdf5a16be7363edca4148af
Terry chan <napoler2008@gmail.com>
2020-06-08 01:50:43 +0800

    Signed-off-by: Terry chan <napoler2008@gmail.com>

--- model_config/run_glue.py
+++ model_config/run_glue.py
@@ -74,7 +74,7 @@ def main():
         model_args, data_args, training_args = parser.parse_json_file(json_file=os.path.abspath(sys.argv[1]))
     else:
         model_args, data_args, training_args = parser.parse_args_into_dataclasses()
-    
+    print("model_args.cache_dir",model_args.cache_dir)
     if (
         os.path.exists(training_args.output_dir)
         and os.listdir(training_args.output_dir)
